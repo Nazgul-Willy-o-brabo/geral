1480187e8a4e0f32e07baa7972f453674abd8306
Willy Campos <willyc.campos71271@gmail.com>
2023-03-11 20:14:00 -0300

    Catch

---
+++ Codigos Gerais/Codigos/TryCatchBank/TryCatchBank/Entities/Conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TryCatchBank.Entities.Exceptions;
+
+namespace TryCatchBank.Entities
+{
+    internal class Conta
+    {
+        private int _numCnt;
+        private String _nomeCli;
+        private Double _saldo;
+        private Double _limite; //Limite p. saldo
+
+
+        public int NumCnt { get => _numCnt; set => _numCnt = value; }
+        public string NomeCli { get => _nomeCli; set => _nomeCli = value; }
+        public double Saldo { get => _saldo; set => _saldo = value; }
+        public double Limite { get => _limite; set => _limite = value; }
+
+
+
+        public Conta() { }
+        public Conta(int numCnt, string nomeCli, double saldo, double limite)
+        {
+            if(saldo <= 0)
+            {
+                throw new Exception("Valor de saldo invalido!\nSolução: Insira ao menos algum valor de saldo incial!");
+            }
+            NumCnt = numCnt;
+            NomeCli = nomeCli;
+            Saldo = saldo;
+            Limite = limite;
+        }
+
+
+        public void Deposito(Double valor) {
+        Saldo += valor;
+        }
+        public void Saque(Double valor) {
+        if(Saldo <= 0)
+            {
+                throw new Exception("Saldo zerado ou negativado\nSolução: É necessario saldo em conta");
+            }
+        if(valor > Limite)
+            {
+                throw new Exception("Não é possivel sacar pois ultrapassa o limite!\nSolução: Tente um valor menor");
+            }
+        if((Saldo -= valor) < 0)
+            {
+                throw new Exception("Não há saldo suficiente!");
+            }
+        Saldo -= valor;
+        }
+    }
+}
---
+++ Codigos Gerais/Codigos/TryCatchBank/TryCatchBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using TryCatchBank.Entities;
+using TryCatchBank.Entities.Exceptions;
+
+try { 
+Console.WriteLine("Entre com uma conta!");
+Console.Write("\nNumero: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Saldo Inicial: ");
+double salIni = double.Parse(Console.ReadLine());
+Console.Write("Limite de Saque: ");
+double Lim = double.Parse(Console.ReadLine());
+Conta conta = new Conta(num,nome,salIni,Lim);
+
+Console.Write("\nEntre com um valor para sacar: ");
+double valSaq = double.Parse(Console.ReadLine());
+conta.Saque(valSaq);
+Console.WriteLine("Novo saldo: " + conta.Saldo);
+} catch (Exception e)
+{
+    Console.WriteLine("Erro inesperado: " + e.Message);
+}
+
+
+
