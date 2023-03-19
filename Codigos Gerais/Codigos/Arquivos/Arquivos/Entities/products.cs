7a73bdb077369736890e2df225d051d871b498f4
Willy Campos <willyc.campos@outlook.com>
2023-03-19 11:41:52 -0300

    Arquivos

---
+++ Codigos Gerais/Codigos/Arquivos/Arquivos/Entities/products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivos.Entities
+{
+    internal class products
+    {
+        public string nome { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+
+        public products() { }
+        public products(string nome, double preco, int qtd) {
+        this.nome = nome;
+        this.preco = preco;
+        quantidade= qtd;
+        }
+
+        public double ValorTotal(double preco, int qtd)
+        {
+            return preco * qtd;
+        }
+    }
+}
---
+++ Codigos Gerais/Codigos/Arquivos/Arquivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using Arquivos.Entities;
+using System.Globalization;
+using System.Linq.Expressions;
+
+string OriginalPath = @"C:\temp";
+string TargetPath = OriginalPath + @"\Out\Summary.csv";
+
+try
+{
+    string[]lines = File.ReadAllLines(OriginalPath+@"\dados.csv");
+    using (StreamWriter sw = File.AppendText(TargetPath))
+    {
+        foreach(string line in lines)
+        {
+            string[] campos = line.Split(';');
+            string nome = campos[0];
+            double preco = double.Parse(campos[1],CultureInfo.InvariantCulture);
+            int qtd = int.Parse(campos[2]);
+
+            products product = new products(nome, preco, qtd);
+            
+            sw.WriteLine(product.nome + "," +product.ValorTotal(preco,qtd));
+        }
+    }
+}catch(Exception e) 
+{
+    Console.WriteLine("Erro: "+e.Message);
+}
