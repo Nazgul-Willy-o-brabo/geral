ae35961249dd414e9f97c24afd1c83cf64829c14
Willy Campos <101759293+Willy-Campos-Costa@users.noreply.github.com>
2023-03-29 11:33:07 -0300

    Update d,.d

---
+++ Codigos Gerais/Codigos/D.D/D.D/Entities/Carta.cs
@@ -0,0 +1,22 @@
+﻿namespace D.D.Entities
+{
+    abstract public class Carta
+    {
+        public int id { get; set; } 
+        public string nome { get; set; }
+        public string desc { get; set; }
+
+        protected Carta(int id,string nome, string desc)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.desc = desc;
+        }
+        protected Carta() { }
+
+        public override string ToString()
+        {
+            return $"Id: {id}\nNome: {nome}\nDescrição: {desc}";
+        }
+    }
+}
---
+++ Codigos Gerais/Codigos/D.D/D.D/Entities/Magica.cs
@@ -0,0 +1,17 @@
+﻿namespace D.D.Entities
+{
+    abstract public class Magica : Carta
+    {
+        public string efeito { get; set; }
+        protected Magica(int id, string nome, string desc, string efeito) : base (id,nome,desc) 
+        { this.efeito = efeito; }
+        protected Magica() { }
+
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\nTipo: Mágica\nEfeito: {efeito}";
+        }
+    }
+
+}
